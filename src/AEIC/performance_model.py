baa64421d64f05852ca43f38a90cc1ff996aac34
Aditeya Shukla <aditeyashukla@gmail.com>
2025-02-21 14:11:26 -0500

    added numpy structured arrays + added config reader

--- src/AEIC/performance_model.py
+++ src/AEIC/performance_model.py
@@ -1,12 +1,22 @@
+import numpy as np
+import toml 
 class PerformanceModel:
     '''Performance model for an aircraft. Contains
         fuel flow, airspeed, ROC/ROD, LTO emissions,
         and OAG schedule'''
 
-    def __init__(self, config=None):
-        self.config = config
+    def __init__(self, config_file_loc=None):
+        self.dtype = [
+            ('h', float),
+            ('airspeed', float),
+            ('gamma', float),
+            ('fuel_mass', float),
+            ('fuel_rate', float)
+        ]
+        with open(config_file_loc, 'r') as f:
+            self.config = toml.load(f)
+        self.states = np.empty(0, dtype=self.dtype)
         self.initialize()
-        pass
 
     def initialize(self):
         '''Uses config file to setup performance
@@ -41,5 +51,11 @@ class PerformanceModel:
         pass
 
     def read_config(self):
-        '''Reads and stores config data'''
-        pass
+       """
+        Reads through each section/key in self.config
+        and sets them as attributes on the class instance.
+        """
+       for _, params in self.config.items():
+            # Each 'params' is another dict of key-value pairs
+            for key, value in params.items():
+                setattr(self, key, value)
