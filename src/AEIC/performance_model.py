fa7839baccbccdcf79c4677f7fa10f2ddf492cc2
Aditeya Shukla <aditeyashukla@gmail.com>
2025-03-24 10:01:17 -0400

    added performance model file with state array, and added config file and reader

--- src/AEIC/performance_model.py
+++ src/AEIC/performance_model.py
@@ -1,61 +1,24 @@
 import numpy as np
-import toml 
+import toml
 class PerformanceModel:
     '''Performance model for an aircraft. Contains
         fuel flow, airspeed, ROC/ROD, LTO emissions,
         and OAG schedule'''
 
-    def __init__(self, config_file_loc=None):
+    def __init__(self, config_file_loc="./IO/default_config.toml"):
+        # Set headers for state variable NumPy structured array
         self.dtype = [
-            ('h', float),
-            ('airspeed', float),
-            ('gamma', float),
-            ('fuel_mass', float),
-            ('fuel_rate', float)
+            ('t', float), # time (s)
+            ('h', float), # Altitude (ft)
+            ('gamma', float), # Flight Path angle: Cruise gamma =0, climb gamma >0, descent gamma < 0
+            ('airspeed', float), # TAS 
+            ('fuel_mass', float), # Mass of fuel
+            ('fuel_rate', float) # Fuel flow rate
         ]
+        # Read config file and store all variables in self.config
+        self.config = {}
         with open(config_file_loc, 'r') as f:
-            self.config = toml.load(f)
+            config_data = toml.load(f)
+            self.config = {k: v for subdict in config_data.values() for k, v in subdict.items()}
+        # Initialize state variables as a numpy structured array
         self.states = np.empty(0, dtype=self.dtype)
-        self.initialize()
-
-    def initialize(self):
-        '''Uses config file to setup performance
-           model data'''
-        
-        if self.config is None:
-            print("No config file provided - will not intialize")
-            pass
-        else:
-            print("Reading configuration file ...")
-            self.read_config()
-
-            print("Creating Schedule ... ")
-            self.create_schedule()
-
-            print("Reading LTO data ... ")
-            self.read_LTO()
-
-            print("Reading Non LTO data ... ")
-            self.read_non_LTO()
-
-    def read_LTO(self):
-        '''Reads and stores LTO data'''
-        pass
-
-    def read_non_LTO(self):
-        '''Reads and stores non LTO data'''
-        pass
-
-    def create_schedule(self):
-        '''Reads and stores OAG Schedule data'''
-        pass
-
-    def read_config(self):
-       """
-        Reads through each section/key in self.config
-        and sets them as attributes on the class instance.
-        """
-       for _, params in self.config.items():
-            # Each 'params' is another dict of key-value pairs
-            for key, value in params.items():
-                setattr(self, key, value)
